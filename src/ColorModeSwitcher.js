12429fe38231568526e204f3753c1a93e20e8d35
nikita-gade12 <nikitagade07@gmail.com>
2023-10-07 23:14:37 +0530

    first commit

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -4,20 +4,23 @@ import { FaMoon, FaSun } from 'react-icons/fa';
 
 export const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
-  const text = useColorModeValue('dark', 'light');
+ 
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
+     
       variant="ghost"
       color="current"
-      marginLeft="2"
+      top={'4'}
+      right={'4'}
+      pos={'fixed'}
       onClick={toggleColorMode}
+      zIndex={'overlay'}
       icon={<SwitchIcon />}
       {...props}
     />
   );
 };
+
+export default ColorModeSwitcher;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,78 @@
+import { Box, Button, HStack, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+import {AiFillGoogleCircle, AiFillInstagram, AiFillYoutube, AiOutlineSend} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'}
+    minH={'40'}
+    p='16'
+    color={'white'}
+    >
+        <Stack direction={['column','row']}>
+          <VStack alignItems={'stretch'}w={'full'}  px={'4'}>
+            <Heading size='md' textTransform={'uppercase'} textAlign={['center', 'left']} >
+                Subscribr to get updates
+            </Heading>
+            <HStack
+            borderBottom={'2px solid white '}
+            py={'2'}
+            
+            >
+              <Input placeholder={'Enter Email Here...'}
+              border={'none'}
+              borderRadius={'none'}
+              outline={'none'}
+              focusBorderColor='none'
+              />
+                <Button
+                p={'0'}
+                colorScheme={'purple'}
+                variant={'ghost'}
+                borderRadius={'0 20px 20px 0'}
+                >
+                    <AiOutlineSend size={'20'}/>
+                </Button>
+            </HStack>
+          </VStack>
+          <VStack w={'full'} 
+          borderLeft={['none','1px solid white']}
+          borderRight={['none','1px solid white']}
+          > 
+          <Heading  textTransform={'uppercase'}textAlign={'center'}>
+            VIDEO HUB
+          </Heading>
+          <Text>All Right Received</Text>
+          </VStack>
+
+           <VStack w={'full'} >
+            <Heading size={'md'} textTransform={'uppercase'} >
+              Social Media
+              </Heading> 
+              <Button variant={'link'} colorScheme={'purple'}>
+                <a target={'black'} href='https://youtube.com'>
+                  < AiFillYoutube color={'white'} size={'2rem'}/>
+                
+                </a>
+              </Button>
+              <Button variant={'link'} colorScheme={'purple'}>
+                <a target={'black'}  href='https://instagram.com'>
+                  <  AiFillInstagram color={'white'} size={'2rem'}/>
+                
+                </a>
+              </Button>
+              <Button variant={'link'} colorScheme={'purple'}>
+                <a  target={'black'} href='https://google.com'>
+                  <   AiFillGoogleCircle color={'white'} size={'2rem'}/>
+                  
+                </a>
+              </Button>
+            </VStack>
+
+        </Stack>
+
+    </Box>
+  )
+}
+
+export default Footer
